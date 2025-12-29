8371251f52625998c9ac7d0b4f2bc40ae5fa4085
Nomah S <3rdtimecoder@gmail.com>
2025-12-29 23:02:55 +0200

    feat: strategy setup

--- server/ai-service/config/config.py
+++ server/ai-service/config/config.py
@@ -7,4 +7,5 @@ if not os.environ.get("OPENAI_API_KEY"):
     os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
 
 embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+
 model = ChatOpenAI(model="gpt-4.1")
