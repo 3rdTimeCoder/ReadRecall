a8364921f1e45db8096b44b5c9454cbbfb9fcb72
Nomah S <3rdtimecoder@gmail.com>
2026-02-16 21:25:20 +0200

    feat: readrecall setup

--- client/readrecall/src/components/recall/Recall.tsx
+++ client/readrecall/src/components/recall/Recall.tsx
@@ -1,13 +1,21 @@
 import React, { type JSX } from "react";
 import { RecallContainer } from "./Recall.styles";
 import SearchBar from "./search-bar/SearchBar";
+import { useReadRecall } from "../../lib/hooks/use-readrecall";
 
 
 const Recall = (): JSX.Element => {
+    const { recall } = useReadRecall();
+
+    const onSearch = async (query: string): Promise<void> => {
+        console.log('query:', query);
+        const res = recall(query);
+        console.log('res:', res);
+    }
 
     return (
         <RecallContainer>
-            <SearchBar onSearch={()=>{}} />
+            <SearchBar onSearch={onSearch} />
         </RecallContainer>
     )
 }
