d35528be7e81a97ae4f28c02cd257bd3e044d25e
3rdtimecoder <3rdtimecoder@gmail.com>
2026-08-11 09:42:31 +0200

    feat: restructure code

---
+++ ingestion/ingestion_pipeline.py
@@ -0,0 +1,48 @@
+from typing import Union
+from pathlib import Path
+from ingestion.loaders import load_docs_from_dir
+from ingestion.chunking import chunk_docs
+from ingestion.embedding import create_vector_store, persist_chunks
+
+
+def ingest_books(
+        dir_path: Union[str, Path], 
+        type="epub", 
+        persist_dir="db/chroma_db",
+        collection="readrecall-book-chunks",
+        is_initial_ingest=True,
+    ) -> bool:
+    """TODO: write docstring"""
+    print("initiating book ingestion...")
+
+    try:
+        #1. Load files
+        files = load_docs_from_dir(dir=dir_path, type=type)
+
+        #2. Chunk files
+        chunks = chunk_docs(files, debug_mode=True)
+
+        #3. Embed and Store files in vector DB
+        if is_initial_ingest: 
+            _ = create_vector_store(
+                chunks=chunks,
+                persist_dir=persist_dir,
+                collection=collection
+            )
+        else:
+            _ = persist_chunks(
+                chunks=chunks,
+                persist_dir=persist_dir,
+                collection=collection
+            )
+    except Exception as e:
+        print(f"Error occurred while ingesting books: {e}")
+        return False
+    
+    return True
+
+
+if __name__ == "__main__":
+    completed = ingest_books("./test-docs")
+    if completed: print("Book ingestion completed successfully!")
+    else: print("Oops! Something went wrong broke in the pipeline. Book ingestion unsuccessful.")
---
+++ retrieval/retrievers.py
@@ -0,0 +1,27 @@
+from typing import Tuple
+from langchain_core.documents import Document
+from ingestion.embedding import load_vector_store
+
+
+def get_retriever(k=5):
+    vector_store = load_vector_store()
+    retriever = vector_store.as_retriever(
+        search_type="similarity_score_threshold",
+        search_kwargs={
+            "k": k,
+            "score_threshold": 0.3
+        }
+    )
+
+    return retriever
+
+
+def query_db(query: str, k=5, threshold=0.3) -> list[Tuple[Document, float]]:
+    vector_store = load_vector_store()
+    results = vector_store.similarity_search_with_score(
+        query=query,
+        k=k
+    )
+    results_above_threshold = [(chunk, score) for chunk,score in results if score >= threshold]
+
+    return results_above_threshold
